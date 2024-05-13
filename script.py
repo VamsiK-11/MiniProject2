d8067a9bccd0e502df1ea953f62af6c6cfc222e8
K Vamsi Krishna <120551373+VamsiK-11@users.noreply.github.com>
2024-05-13 10:31:44 +0530

    script.py

--- script.py
+++ script.py
@@ -5,12 +5,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 chrome_driver_path = "C:\Windows\System32\chromedriver-win64\chromedriver.exe"
-
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"  # Specify path to Chrome binary if needed
 chrome_options.add_argument(f"webdriver.chrome.driver={chrome_driver_path}")
-
 driver = webdriver.Chrome(options=chrome_options)
+
 
 driver.get("https://thinking-tester-contact-list.herokuapp.com/")
 
@@ -26,11 +25,38 @@ try:
     WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located((By.ID, "add-contact"))
     )
-    screenshot = Image.open("add_contact_button.png")
 
-    # Display the screenshot
-    screenshot.show()
+    add_contact_button = driver.find_element(By.ID, "add-contact")
+    add_contact_button.click()
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, "add-contact"))
+        )
+    
+    name_field = driver.find_element(By.ID, "firstName")
+    name2_field = driver.find_element(By.ID,"lastName")
+    email_field = driver.find_element(By.ID, "email")
+    dob_field = driver.find_element(By.ID, "birthdate")
+    phone_field = driver.find_element(By.ID, "phone")
+    address_field = driver.find_element(By.ID, "street1")
 
+    name_field.send_keys("Glenn")
+    name2_field.send_keys("Phillips")
+    email_field.send_keys("phillips23@gmail.com")
+    dob_field.send_keys("1996-12-06")
+    phone_field.send_keys("8945612379")
+    address_field.send_keys("Auckland,New Zealand")
+
+    submit_button = driver.find_element(By.ID, "submit")
+    submit_button.click()
+    
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "myTable"))
+        )
+    
+    contact_add = driver.find_element(By.ID,"myTable")
+    screenshot2 = contact_add.screenshot("result.png")
+    screenshot2 = Image.open("result.png")
+    screenshot2.show()
     print("Test Passed: Login successful!")
 
 except Exception as e:
